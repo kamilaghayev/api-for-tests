eb1b71a01b54b75ec88f53667894b1946284e9ff
Kamil Aghayev <kamilizundef@gmail.com>
2025-04-03 15:44:52 +0400

    `Update PORT variable to use environment variable if set`

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 6060;
+const PORT = process.env.PORT || 6060;
 const COMMENTS_FILE = path.join(__dirname, 'comments.json');
 
 app.use(bodyParser.json());
